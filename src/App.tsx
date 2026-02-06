fe3b44b1a4e919dcc0a8f0e23273bd900fda8a90
Blacksider1 <adamtsurov123@gmail.com>
2026-02-06 12:00:31 +0300

    feat: refactor app structure and styles
    - Added SCSS support and replaced App.css with App.scss for better styling management.
    - Implemented a sidebar for filtering skills with responsive design.
    - Introduced new SVG icons for the sidebar functionality.
    - Updated package.json to include sass and vite-plugin-svgr.
    - Enhanced TypeScript configuration with path aliases.
    - Removed unused CSS styles and optimized global styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import FiltersOpenButtonIcon from '@/assets/icons/open-skills-side.svg?react'
+import FiltersSearchInputIcon from '@/assets/icons/search-skills-side.svg?react'
+import FigmaSkillIcon from '@/assets/icons/skillIcons/figma-skill-icon.svg?react'
+import { useRef, useState } from 'react'
+
+const COLLAPSED_HEIGHT = 200
+const EXPANDED_HEIGHT = 500
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showFilters, setShowFilters] = useState(false)
+  const [showAllSkills, setShowAllSkills] = useState(false)
+  const [showOpenAllSkillsButton, setShowOpenAllSkillsButton] = useState(false)
+  const skillListRef = useRef(null)
+
+  const toggleVisibleSidebar = () => {
+    setShowFilters((prev) => !prev)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app">
+      <main className="main">
+        <section className="list-questions">
+          <div className="list-title-wrapper">
+            <h2 className="list-questions-title">Вопросы React, JavaScript</h2>
+            <button className={`filters-sidebar-open-button ${showFilters ? 'open' : ''}`}>
+              <FiltersOpenButtonIcon className="skills-open-icon" onClick={toggleVisibleSidebar} />
+            </button>
+          </div>
+        </section>
+        <aside className={`filters-sidebar ${showFilters ? 'open' : ''}`}>
+          <button className="filters-sidebar-close-button" onClick={toggleVisibleSidebar}>
+            <FiltersOpenButtonIcon className="filters-close-icon" />
+          </button>
+          <label htmlFor="skill-input" className="filters-search-input">
+            <FiltersSearchInputIcon className="filters-search-icon" />
+            <input id="skill-input" type="text" placeholder="Введите запрос..." />
+          </label>
+          <div className="skills-question">
+            <span className="skills-side-title">Категории вопросов</span>
+            <div className="skills">
+              <button className="skill-button">
+                <FigmaSkillIcon />
+                Figma
+              </button>
+              <button className="skill-button">
+                {/* <svg /> */}
+                React.js
+              </button>
+              <button className="skill-button">
+                <FigmaSkillIcon />
+                Wireframing
+              </button>
+              <button className="skill-button">
+                {/* <svg /> */}
+                Figma
+              </button>
+            </div>
+          </div>
+        </aside>
+      </main>
+    </div>
   )
 }
 
