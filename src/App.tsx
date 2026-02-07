59e64ec6277b2f07e5d3eaec9726758a1020c52f
Blacksider1 <adamtsurov123@gmail.com>
2026-02-07 22:38:16 +0300

    feat: implement expandable skills list and sticky sidebar styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,69 @@ import './App.scss'
 import FiltersOpenButtonIcon from '@/assets/icons/open-skills-side.svg?react'
 import FiltersSearchInputIcon from '@/assets/icons/search-skills-side.svg?react'
 import FigmaSkillIcon from '@/assets/icons/skillIcons/figma-skill-icon.svg?react'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
-const COLLAPSED_HEIGHT = 200
+const COLLAPSED_HEIGHT = 120
 const EXPANDED_HEIGHT = 500
 
 function App() {
   const [showFilters, setShowFilters] = useState(false)
-  const [showAllSkills, setShowAllSkills] = useState(false)
-  const [showOpenAllSkillsButton, setShowOpenAllSkillsButton] = useState(false)
-  const skillListRef = useRef(null)
+  const [isOpen, setIsOpen] = useState(false)
+  const [showExpandBtn, setShowExpandBtn] = useState(false)
+  const skillListRef = useRef<HTMLDivElement>(null)
+
+  const skills = [
+    { id: 1, title: 'Figma' },
+    { id: 2, title: 'Wireframing' },
+    { id: 3, title: 'React' },
+    { id: 4, title: 'Wireframing' },
+    { id: 5, title: 'Figma' },
+    { id: 6, title: 'React' },
+    { id: 5, title: 'Figma' },
+    { id: 6, title: 'React' },
+    { id: 5, title: 'Figma' },
+    { id: 6, title: 'React' },
+    
+    { id: 5, title: 'Figma' },
+    { id: 6, title: 'React' },
+    { id: 5, title: 'Figma' },
+    { id: 6, title: 'React' },
+    { id: 5, title: 'Figma' },
+    { id: 6, title: 'React' },
+    { id: 5, title: 'Figma' },
+    { id: 6, title: 'React' },
+    { id: 5, title: 'Figma' },
+    { id: 6, title: 'React' },
+    { id: 5, title: 'Figma' },
+    { id: 6, title: 'React' },
+      { id: 5, title: 'Figma' },
+      { id: 6, title: 'React' },
+      { id: 5, title: 'Figma' },
+      { id: 6, title: 'React' },
+      { id: 5, title: 'Figma' },
+      { id: 6, title: 'React' },
+      { id: 5, title: 'Figma' },
+      { id: 6, title: 'React' },
+
+  ]
 
   const toggleVisibleSidebar = () => {
     setShowFilters((prev) => !prev)
   }
+
+  useEffect(() => {
+    const checkHeight = () => {
+      if (skillListRef.current) {
+        const isToBig = skillListRef.current.scrollHeight > COLLAPSED_HEIGHT
+        setShowExpandBtn(isToBig)
+      }
+    }
+    checkHeight()
+
+    window.addEventListener('resize', checkHeight)
+
+    return () => window.removeEventListener('resize', checkHeight)
+  }, [skills])
 
   return (
     <div className="app">
@@ -27,6 +76,13 @@ function App() {
               <FiltersOpenButtonIcon className="skills-open-icon" onClick={toggleVisibleSidebar} />
             </button>
           </div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur quas veritatis eaque in neque ex suscipit aliquid odit facilis esse illo, nulla est ducimus dolores sit minima laborum temporibus error.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur quas veritatis eaque in neque ex suscipit aliquid odit facilis esse illo, nulla est ducimus dolores sit minima laborum temporibus error.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur quas veritatis eaque in neque ex suscipit aliquid odit facilis esse illo, nulla est ducimus dolores sit minima laborum temporibus error.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur quas veritatis eaque in neque ex suscipit aliquid odit facilis esse illo, nulla est ducimus dolores sit minima laborum temporibus error.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur quas veritatis eaque in neque ex suscipit aliquid odit facilis esse illo, nulla est ducimus dolores sit minima laborum temporibus error.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur quas veritatis eaque in neque ex suscipit aliquid odit facilis esse illo, nulla est ducimus dolores sit minima laborum temporibus error.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur quas veritatis eaque in neque ex suscipit aliquid odit facilis esse illo, nulla est ducimus dolores sit minima laborum temporibus error.
         </section>
         <aside className={`filters-sidebar ${showFilters ? 'open' : ''}`}>
           <button className="filters-sidebar-close-button" onClick={toggleVisibleSidebar}>
@@ -38,24 +94,60 @@ function App() {
           </label>
           <div className="skills-question">
             <span className="skills-side-title">Категории вопросов</span>
-            <div className="skills">
-              <button className="skill-button">
-                <FigmaSkillIcon />
-                Figma
-              </button>
-              <button className="skill-button">
-                {/* <svg /> */}
-                React.js
-              </button>
-              <button className="skill-button">
-                <FigmaSkillIcon />
-                Wireframing
-              </button>
-              <button className="skill-button">
-                {/* <svg /> */}
-                Figma
-              </button>
+            <div
+              className="skills"
+              ref={skillListRef}
+              style={{
+                maxHeight: isOpen ? '200px' : `${COLLAPSED_HEIGHT}px`,
+                overflow: 'hidden',
+                overflowY: isOpen ? 'auto' : 'hidden',
+                transition: 'max-height 0.3s ease-in-out',
+              }}
+            >
+              {skills.map((skill) => (
+                <button className="skill-button">
+                  <FigmaSkillIcon />
+                  {skill.title}
+                </button>
+              ))}
             </div>
+            {showExpandBtn && (
+              <button className={'skillsExpandButton'} onClick={() => setIsOpen(!isOpen)}>
+                {isOpen ? 'Свернуть' : 'Показать всё'}
+              </button>
+            )}
+          </div>
+          <button className="filters-sidebar-close-button" onClick={toggleVisibleSidebar}>
+            <FiltersOpenButtonIcon className="filters-close-icon" />
+          </button>
+          <label htmlFor="skill-input" className="filters-search-input">
+            <FiltersSearchInputIcon className="filters-search-icon" />
+            <input id="skill-input" type="text" placeholder="Введите запрос..." />
+          </label>
+          <div className="skills-question">
+            <span className="skills-side-title">Категории вопросов</span>
+            <div
+              className="skills"
+              ref={skillListRef}
+              style={{
+                maxHeight: isOpen ? '400px' : `${COLLAPSED_HEIGHT}px`,
+                overflow: 'hidden',
+                overflowY: isOpen ? 'auto' : 'hidden',
+                transition: 'max-height 0.3s ease-in-out',
+              }}
+            >
+              {skills.map((skill) => (
+                <button className="skill-button">
+                  <FigmaSkillIcon />
+                  {skill.title}
+                </button>
+              ))}
+            </div>
+            {showExpandBtn && (
+              <button className={'skillsExpandButton'} onClick={() => setIsOpen(!isOpen)}>
+                {isOpen ? 'Свернуть' : 'Показать всё'}
+              </button>
+            )}
           </div>
         </aside>
       </main>
